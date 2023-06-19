5d106612d2ebb491883f349facb704b511c57961
Chip Hardy <chiphardy1995@icloud.com>
2023-06-19 14:34:16 -0500

    V0.0.3 Fixed function names for calcPowerLvv and calcNumLvl Set the Log in calcNumLvl to 10 instead of a natural log Added UI for entering character stats Added textline output for vewing character ranking Added a newline at the begining to the menu for better readability Added TODOs for better veiw of future development

--- Strength_calc.py
+++ Strength_calc.py
@@ -1,6 +1,9 @@
 #Ronald Hardy Jr
-# V0.0.1 6/10/2023
+# V0.0.2 6/18/2023
 import math
+# TODO: Setup app using flask
+# TODO: intergrate sql with
+# TODO: create docker image & run in container
 
 """class character:
     # TODO: Finish character class for saving characters
@@ -56,27 +59,23 @@ def calcMental(iq, biq, knowl): return (0.25 * iq) + (0.25 * biq) + (0.25 * know
 def calcMagical(manaCap, maxOutput,regenRate): return (0.0004 * manaCap) + (0.0004 * maxOutput) + (0.004 * regenRate)
 
 def calcPowerLvl(phsLvl, magLvl, mentLvl): return (0.45 * magLvl) + (0.45 * phsLvl) + (0.1 * mentLvl)
-def calcPowerLvl(offenStrn, defenStrn, speed, adgility, manaCap, maxOutput,regenRate, iq, biq, knowl):
+def calcPowerLvl_usingStats(offenStrn, defenStrn, speed, adgility, manaCap, maxOutput,regenRate, iq, biq, knowl):
     phsLvl = calcPhysical(offenStrn, defenStrn, speed, adgility)
     magLvl = calcMagical(manaCap, maxOutput,regenRate)
     mentlvl = calcMental(iq, biq, knowl)
+    #return (0.45 * calcMagical(manaCap, maxOutput,regenRate)) + (0.45 * calcPhysical(offenStrn, defenStrn, speed, adgility)) + (0.1 * calcMental(iq, biq, knowl))
     return calcPowerLvl(phsLvl, magLvl, mentlvl)
-    #return calcPowerLvl(calcPhysical(offenStrn, defenStrn, speed, adgility),calcMagical(manaCap, maxOutput,regenRate),calcMental(iq, biq, knowl))
-    raise NotImplementedError("calcPowerLvl (7 inputs) is to implemented at a later date")
 
 def calcNumLvl(powerLvl):
-    return 7.15 * math.log(powerLvl) - 4.3
-    raise NotImplementedError("callcNumLvl (1 input) is to be implemented at a later date")
-def calcNumLvl(phsLvl, magLvl, mentLvl):
+    return 7.15 * math.log(powerLvl, 10) - 4.3
+def calcNumLvl_noPwrLvl(phsLvl, magLvl, mentLvl):
     powerLvl = calcPowerLvl(phsLvl, magLvl, mentLvl)
     return calcNumLvl(powerLvl)
     #return calcNumLvl(calcPowerLvl(phsLvl, magLvl, mentLvl))
-    raise NotImplementedError("callcNumLvl (3 inputs) is to be implemented at a later date")
-def calcNumLvl(offenStrn, defenStrn, speed, adgility, manaCap, maxOutput,regenRate, iq, biq, knowl):
+def calcNumLvl_usingStats(offenStrn, defenStrn, speed, adgility, manaCap, maxOutput,regenRate, iq, biq, knowl):
     powerLvl = calcPowerLvl(offenStrn, defenStrn, speed, adgility, manaCap, maxOutput,regenRate, iq, biq, knowl)
     return calcNumLvl(powerLvl)
     #return calcNumLvl(calcPowerLvl(offenStrn, defenStrn, speed, adgility, manaCap, maxOutput,regenRate, iq, biq, knowl))
-    raise NotImplementedError("callcNumLvl (7 inputs) is to be implemented at a later date")
 
 def readFile():
     # TODO: file read and write
@@ -86,10 +85,14 @@ def saveToFile():
 
 #def StrenthCalc():
 if __name__ == "__main__":
+    #Tesing
+
+    #UI
     # TODO: run user interface for opitions and inputing data
-    print("Weolcome to Character Strength Calculator! \nUsing the CH Base Magic System's Power Level Rank!\n")
+    # TODO: add character arraylist and ability to add, remove, view, and edit chacters on it
+    print("Weolcome to Character Strength Calculator! \nUsing the CH Base Magic System's Power Level Rank!")
     option = -1
-    menu = "0: Quit\n1: Input Character Stats\n2: View Character Stats & Levels\n3: Edit Chacter Stats\n4: Save Character Data\n5: Load data from file"
+    menu = "\n0: Quit\n1: Input Character Stats\n2: View Character Stats & Levels\n3: Edit Chacter Stats\n4: Save Character Data\n5: Load data from file"
     while option != 0:
         try:
             print(menu)
@@ -97,7 +100,25 @@ if __name__ == "__main__":
             option = int(input())
             match option:
                 case 0: break
-                case 1: print("Placeholder") #Temp, delete when implementing
+                case 1:
+                    # TODO: Add inputs for character stats and use inputs to return character levels
+                    offenStrn = int (input("Please enter the character's Offensive Strength (Numbers Only): "))
+                    defenStrn = int(input("Please enter the character's Defensive Strength (numbers only): "))
+                    speed = int(input("Please enter the character's speed in km/hr: "))
+                    adgility = int(input("Please enter the character's avg running acceleration in m/s: "))
+                    manaCap = int(input("Please enter the character's maximun mana capacity in mp: "))
+                    maxOutput = int(input("Please enter the character's maximum mana output in mp: "))
+                    regenRate = int(input("Please enter the character's mana regeneration rate in mp/hr: "))
+                    iq = int(input("Please enter the character's IQ: "))
+                    biq = int(input("Please enter the character's battle iq (0-100, avg 50): "))
+                    knowl = int(input("Please enter the character's score on a world magic & history test (0-100, avg 50): "))
+                    phsLvl = calcPhysical(offenStrn, defenStrn, speed, adgility)
+                    magLvl = calcMagical(manaCap, maxOutput,regenRate)
+                    mentLvl = calcMental(iq, biq, knowl)
+                    powerLvl = calcPowerLvl(phsLvl, magLvl, mentLvl)
+                    numRankLvl = calcNumLvl(powerLvl)
+                    print(f'This Character has a Number Rank of: {numRankLvl:.2f}\nTheir Power level: {powerLvl:.0f}')
+                    print(f'Other Character Stats:\nMagical Power: {magLvl:.0f}\nPhysical Level: {phsLvl:.0f}\nMental Power: {mentLvl:.0f}')
                 case 2: raise NotImplementedError
                 case 3: raise NotImplementedError
                 case 4: raise NotImplementedError
@@ -110,5 +131,3 @@ if __name__ == "__main__":
         except ValueError:
             print("Input is not correct. Please input a number.")
     print("Thanks for using Character Strength Calculator! have a good day!")
-
-    #print(option) #for debug only
